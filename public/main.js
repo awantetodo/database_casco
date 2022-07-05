da226f33cd8c46958fa3012be2744c051417c576
awantetodo <ignaciocasco@hotmail.com>
2022-07-04 23:40:53 -0300

    first commit

---
+++ public/main.js
@@ -0,0 +1,87 @@
+const socket=io();
+
+const divChat = document.querySelector('#chat');
+const boton = document.querySelector('#enviar');
+const respuesta = document.querySelector('#warning');
+const btnGuardar= document.querySelector('#guardar');
+const body = document.querySelector('#table-body');
+
+boton.addEventListener("click",(event)=>{
+    respuesta.innerHTML=" ";
+    const usuario = document.querySelector('#user').value;
+    if( /(.+)@(.+){2,}\.(.+){2,}/.test(usuario) ){
+        const mensaje = document.querySelector('#texto').value;
+        const message={
+            autor:usuario,
+            hora:new Date().toLocaleString(),
+            texto:mensaje
+        }
+        socket.emit('newMessage',message)
+    } else {
+        respuesta.innerHTML="Debe ingresar un mail";
+    } 
+})
+
+btnGuardar.addEventListener("click",(event)=>{
+    const title = document.querySelector('#title').value;
+    const price = document.querySelector('#price').value;
+    const image = document.querySelector('#image').value;
+    if (title !== '' && price !== '' && image !== '') {
+        const producto ={
+            "title": title,
+            "price": price,
+            "image": image
+        }
+        socket.emit('product',producto)
+    }
+
+})
+
+socket.on('messages',(messages)=>{
+    if (messages == null){
+        divChat.innerHTML= " ";
+    } else {
+        divChat.innerHTML=messages.map(message=>{
+            return(
+                `<div>
+                <span style="color:blue; font-weight: bold;">${message.autor}</span>
+                <span style="color:brown">${message.hora}</span>
+                <span style="color:green; font-style: italic;">${message.texto}</span>
+                </div>
+                `
+            )
+        }).join(" ")
+    }
+})
+
+socket.on('newMessages',(messages)=>{
+    divChat.innerHTML = messages.map(message=>{
+        return(
+            `<div>
+            <span style="color:blue; font-weight: bold;">${message.autor}</span>
+            <span style="color:brown">${message.hora}</span>
+            <span style="color:green; font-style: italic;">${message.texto}</span>
+            </div>
+            `
+        )
+    }).join(" ")
+})
+
+socket.on('newProduct',(products)=>{
+    body.innerHTML = " ";
+    if (products == null){
+        body.innerHTML = " ";
+    } else {
+        body.innerHTML = products.map(products =>{
+            return(
+                `<tr>
+                    <td class="table-info">${products.id}</td>
+                    <td class="table-info">${products.title}</td>
+                    <td class="table-info">${products.price}</td>
+                    <td class="table-info"><img src=${products.image} alt="No image" width="20px"/></td>
+                </tr>
+                `
+            )
+        }).join(" ")
+    }
+})
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const {Server: ioServer} = require('socket.io');
+const http = require('http');
+const app = express();
+const httpServer = http.createServer(app);
+const io = new ioServer(httpServer);
+
+const {options} = require('./configDB')
+const contenedorProd = require('./contenedorProd.js');
+const contenedorChat = require('./contenedorChat.js');
+const content = new contenedorProd(options.mariaDB,'productos');
+const chat = new contenedorChat(options.sqlite,'usuarios')
+
+app.use(express.static(__dirname +"/public"))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('views','./views');
+app.set('view engine', 'ejs');
+
+app.get('/',async(req,res)=>{
+    let products = await content.getAll();
+    if (products != null){
+        res.render('index.ejs',{products})
+    } else {
+        products = [];
+        res.render('index.ejs',{products})
+    }
+})
+
+app.post('/productos',async(req,res)=>{
+    res.redirect('/')
+})
+
+io.on('connection',async(socket)=>{
+    console.log('Cliente conectado',socket.id);
+    
+    const mensajes = await chat.getAll();
+    socket.emit('messages', mensajes)
+
+    socket.on('newMessage', async(message)=>{
+        await chat.save(message);
+        const mensajes = await chat.getAll();
+        io.sockets.emit('newMessages', mensajes)
+    })
+    socket.on('product', async(data)=>{
+        await content.save(data);
+        const products = await content.getAll();
+        io.sockets.emit('newProduct', products)
+    })
+})
+
+const PORT=8080;
+httpServer.listen(PORT,()=>{
+    console.log(`Servidor escuchando puerto ${PORT}`);
+});
+
